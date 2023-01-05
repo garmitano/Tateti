c192b95ad9db95a2a57a95d1e5a57783a4e3d62b
Benja <benjaarmic@gmail.com>
2023-01-05 13:29:02 -0300

    fix: Arreglo del anterior commit

---
+++ src/es/games/tateti/views/TurnView.java
@@ -0,0 +1,15 @@
+package es.games.tateti.views;
+
+import es.games.tateti.models.Turn;
+
+public class TurnView {
+	final Turn turn;
+	
+	
+	public TurnView(Turn turn) {
+		this.turn = new Turn();
+	}
+	
+	
+	
+}
